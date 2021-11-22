731cebce0233cf5437aa425cd2994fc895882e7c
luke-mendi02 <lukecmendiola@lewisu.edu>
2021-11-22 00:22:12 -0600

    new files

--- mustangV1.js
+++ mustangV1.js
@@ -1,9 +1,95 @@
 var contactURLArray = [];
 var contactArray = [];
 var loadingContact = 0;
+var currentContactIndex = 0; 
 
+function viewCurrentContact() {
+    currentContact = contactArray[currentContactIndex];
+    console.log(currentContact);
+    document.getElementById("nameID").value = currentContact.preferredName;   
+    document.getElementById("emailID").value = currentContact.email;   
+    document.getElementById("cityID").value = currentContact.city;   
+    document.getElementById("stateID").value = currentContact.state;
+    document.getElementById("zipID").value = currentContact.zip;  
+    document.getElementById("statusID").innerHTML = "Status: Viewing contact " + (currentContactIndex+1) + " of " + contactArray.length;
+}
+
+function previous() {
+    if (currentContactIndex > 0) {
+        currentContactIndex--;
+    }
+    currentContact = contactArray[currentContactIndex];
+    viewCurrentContact();
+}
+
+function next() {
+    if (currentContactIndex < (contactArray.length-1)) {
+        currentContactIndex++;
+    }
+    currentContact = contactArray[currentContactIndex];
+    viewCurrentContact();
+    
+}
+var newContact = {
+    preferredName:document.getElementById("nameID").value,
+    email:document.getElementById("emailID").value,
+    city:document.getElementById("cityID").value,
+    state:document.getElementById("stateID").value,
+    zip:document.getElementById("zipID").value,
+}
+function add() {
+contactArray.push(newContact);
+    console.log('add()');
+}
+
+function remove() {
+    currentContact = contactArray[currentContactIndex];
+    console.log(currentContact);
+    document.getElementById("nameID").value = currentContact.preferredName;   
+    document.getElementById("emailID").value = currentContact.email;   
+    document.getElementById("cityID").value = currentContact.city;   
+    document.getElementById("stateID").value = currentContact.state;
+    document.getElementById("zipID").value = currentContact.zip;  
+    const index = contactArray.indexOf(currentContactIndex);
+if (index > -1) {
+  contactArray.splice(index, 1);
+}
+console.log(contactArray);
+    console.log('remove()');
+
+}
+
+function zipBlurFunction() {
+    getPlace();
+}
+
+function keyPressed() {
+    console.log('keyPressed()');
+}
+
+function getPlace() {
+    var zip = document.getElementById("zipID").value
+    console.log("zip:"+zip);
+
+    console.log("function getPlace(zip) { ... }");
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var result = xhr.responseText;
+            console.log("result:"+result);
+            var place = result.split(', ');
+            if (document.getElementById("cityID").value == "")
+                document.getElementById("cityID").value = place[0];
+            if (document.getElementById("stateID").value == "")
+                document.getElementById("stateID").value = place[1];
+        }
+    }
+    xhr.open("GET", "mustang-v2.php?zip=" + zip);
+    xhr.send(null);
+}
 function initApplication() {
-    console.log('Mustang v1.2'); 
+    console.log('Mustang v2.3'); 
 }
 
 function loadIndex() {
@@ -32,9 +118,6 @@ function loadContacts() {
     }
 }
 
-
-document.getElementById("doneLoadingContactsOnPage").innerHTML = "All contacts are loaded!";
-
 function loadNextContact(URL) {
     console.log("URL: " + URL);
     contactRequest = new XMLHttpRequest();
@@ -45,23 +128,24 @@ function loadNextContact(URL) {
         contact = JSON.parse(contactRequest.responseText);
         console.log("Contact: " + contact.firstName);
         contactArray.push(contact);
-        document.getElementById("contactsID").innerHTML = JSON.stringify(contactArray);
-
         loadingContact++;
         if (contactURLArray.length > loadingContact) {
 			loadNextContact(contactURLArray[loadingContact]);
-			document.getElementById("contactsOnPage").innerHTML = "Still loading contacts...";
+			document.getElementById("contactsOnPage").innerHTML = "Still loading contacts..." + contact.firstName + " " + contact.lastName;
 		}
 		else {
 			document.getElementById("contactsOnPage").innerHTML = "All contacts are loaded!";
 		}
-		
-		
     }
-
     contactRequest.send();
 }
 
 function logContacts() {
     console.log(contactArray);
+}
+function showContacts() {
+    for (i = 0; i < contactArray.length; i++){
+        document.getElementById("contacts").innerHTML += JSON.stringify(contactArray[i]) + "<br/><br />";
+    }
+contactRequest.send();
 }
